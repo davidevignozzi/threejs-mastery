1a44a8a51b3403868cf66fce8d6d527d0bd617ee
Davide Vignozzi <73885963+davidevignozzi@users.noreply.github.com>
2023-06-13 09:36:55 +0200

    :sparkles: Hero

--- threejs-gsap-webgi-drill/src/index.ts
+++ threejs-gsap-webgi-drill/src/index.ts
@@ -30,7 +30,8 @@ import './styles.css';
 async function setupViewer() {
     // Initialize the viewer
     const viewer = new ViewerApp({
-        canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement
+        canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement,
+        useRgbm: false
     });
 
     // Add some plugins
