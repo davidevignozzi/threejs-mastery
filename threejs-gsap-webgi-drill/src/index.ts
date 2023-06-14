a8988d772c494e56188c2ba6acfb3d76118fdcfb
Davide Vignozzi <73885963+davidevignozzi@users.noreply.github.com>
2023-06-14 12:09:38 +0200

    :dizzy: Setup animation

--- threejs-gsap-webgi-drill/src/index.ts
+++ threejs-gsap-webgi-drill/src/index.ts
@@ -2,29 +2,23 @@ import {
     ViewerApp,
     AssetManagerPlugin,
     GBufferPlugin,
-    timeout,
     ProgressivePlugin,
     TonemapPlugin,
     SSRPlugin,
     SSAOPlugin,
-    DiamondPlugin,
-    FrameFadePlugin,
-    GLTFAnimationPlugin,
-    GroundPlugin,
     BloomPlugin,
-    TemporalAAPlugin,
-    AnisotropyPlugin,
     GammaCorrectionPlugin,
-    addBasePlugins,
-    ITexture,
     TweakpaneUiPlugin,
     AssetManagerBasicPopupPlugin,
     CanvasSnipperPlugin,
     IViewerPlugin
-
-    // Color, // Import THREE.js internals
-    // Texture, // Import THREE.js internals
 } from 'webgi';
+
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
 import './styles.css';
 
 async function setupViewer() {
@@ -37,26 +31,21 @@ async function setupViewer() {
     // Add some plugins
     const manager = await viewer.addPlugin(AssetManagerPlugin);
 
+    const camera = viewer.scene.activeCamera;
+    const position = camera.position;
+    const target = camera.target;
+
     // Add a popup(in HTML) with download progress when any asset is downloading.
     await viewer.addPlugin(AssetManagerBasicPopupPlugin);
 
     // Add plugins individually.
-    // await viewer.addPlugin(GBufferPlugin)
-    // await viewer.addPlugin(new ProgressivePlugin(32))
-    // await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
-    // await viewer.addPlugin(GammaCorrectionPlugin)
-    // await viewer.addPlugin(SSRPlugin)
-    // await viewer.addPlugin(SSAOPlugin)
-    // await viewer.addPlugin(DiamondPlugin)
-    // await viewer.addPlugin(FrameFadePlugin)
-    // await viewer.addPlugin(GLTFAnimationPlugin)
-    // await viewer.addPlugin(GroundPlugin)
-    // await viewer.addPlugin(BloomPlugin)
-    // await viewer.addPlugin(TemporalAAPlugin)
-    // await viewer.addPlugin(AnisotropyPlugin)
-
-    // or use this to add all main ones at once.
-    await addBasePlugins(viewer);
+    await viewer.addPlugin(GBufferPlugin);
+    await viewer.addPlugin(new ProgressivePlugin(32));
+    await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm));
+    await viewer.addPlugin(GammaCorrectionPlugin);
+    await viewer.addPlugin(SSRPlugin);
+    await viewer.addPlugin(SSAOPlugin);
+    await viewer.addPlugin(BloomPlugin);
 
     // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
     await viewer.addPlugin(CanvasSnipperPlugin);
@@ -66,17 +55,44 @@ async function setupViewer() {
 
     await manager.addFromPath('./assets/drill.glb');
 
-    // Load an environment map if not set in the glb file
-    // await viewer.scene.setEnvironment(
-    //     await manager.importer!.importSinglePath<ITexture>(
-    //         "./assets/environment.hdr"
-    //     )
-    // );
-
     // Add some UI for tweak and testing.
     const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin);
     // Add plugins to the UI to see their settings.
     uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin);
+
+    function setupScrollAnimation() {
+        const tl = gsap.timeline();
+
+        // From the first section to second section
+        tl.to(position, {
+            x: 5,
+            duration: 4,
+            onUpdate
+            // scrollTrigger: {
+            //     trigger: '#second-section'
+            // }
+        });
+    }
+
+    setupScrollAnimation();
+
+    /**
+     * WEBGI UPDATE
+     */
+    let needsUpdate = true;
+
+    function onUpdate() {
+        needsUpdate = true;
+        viewer.renderer.resetShadows();
+    }
+
+    viewer.addEventListener('preFrame', () => {
+        if (needsUpdate) {
+            camera.positionUpdated(false);
+            camera.targetUpdated(true);
+            needsUpdate = false;
+        }
+    });
 }
 
 setupViewer();
