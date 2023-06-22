7d0df924dc39e02e9114ad5252e29282800577e7
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-06-22 10:58:02 +0200

    :bulb: Initial Light Setup

--- r3f-avatar-experience/src/components/Experience.jsx
+++ r3f-avatar-experience/src/components/Experience.jsx
@@ -1,11 +1,15 @@
-import { OrbitControls } from '@react-three/drei';
+import { Environment, Lightformer, OrbitControls, useHelper } from '@react-three/drei';
 
 import { Avatar } from './Avatar';
 import AvatarController from './AvatarController';
 
 import Ground from './Ground';
+import { useRef } from 'react';
+import { DirectionalLightHelper } from 'three';
 
 export const Experience = () => {
+  const dirLight = useRef();
+  useHelper(dirLight, DirectionalLightHelper, 'red');
   return (
     <>
       <OrbitControls />
@@ -16,7 +20,19 @@ export const Experience = () => {
 
       <Ground />
 
-      <ambientLight intensity={1} />
+      {/* Lights */}
+      {/* <ambientLight intensity={0.2} /> */}
+      <Environment>
+        <Lightformer position={[-3, 1, -1]} scale={5} color="white" intensity={2} form="circle" />
+      </Environment>
+
+      <directionalLight
+        ref={dirLight}
+        position={[-2.5, 2, 5]}
+        intensity={1}
+        castShadow
+        color={'#e3b887'}
+      />
     </>
   );
 };
--- r3f-avatar-experience/src/components/Ground.jsx
+++ r3f-avatar-experience/src/components/Ground.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Ground = () => {
   return (
     <RigidBody type="fixed" friction={1.5}>
-      <Box position={[0, 0, 0]} args={[10, 1, 10]} receiveShadow>
+      <Box position={[0, 0, 0]} args={[8, 0.5, 8]} receiveShadow>
         <meshStandardMaterial color="white" />
       </Box>
     </RigidBody>
