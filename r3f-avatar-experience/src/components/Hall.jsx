0d565401802932bab299efc3ab2756f7759177cc
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-06-22 17:51:11 +0200

    :sparkles: Logo and Timeline

--- r3f-avatar-experience/src/components/Hall.jsx
+++ r3f-avatar-experience/src/components/Hall.jsx
@@ -6,6 +6,8 @@ const experiences = [
   // Boolean
   {
     name: 'boolean',
+    logo: '/images/booleanLogo.png',
+    time: 'October 2020 - July 2021',
     type: 'education',
     roomWallMaterial: new THREE.MeshStandardMaterial({ color: '#ffffff' }),
     roomGroundMaterial: new THREE.MeshStandardMaterial({ color: '#f0f0f0' }),
@@ -15,6 +17,8 @@ const experiences = [
   // Everis
   {
     name: 'everis',
+    logo: '',
+    time: 'October 2021 - october 2022',
     type: 'job',
     roomWallMaterial: new THREE.MeshStandardMaterial({ color: '#ffffff' }),
     roomGroundMaterial: new THREE.MeshStandardMaterial({ color: '#f0f0f0' }),
@@ -33,6 +37,8 @@ const Hall = () => {
             roomWallMaterial={room.roomWallMaterial}
             roomGroundMaterial={room.roomGroundMaterial}
             lightColor={room.lightColor}
+            logo={room.logo}
+            timeline={room.time}
           />
         );
       })}
--- r3f-avatar-experience/src/components/Room.jsx
+++ r3f-avatar-experience/src/components/Room.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { CuboidCollider, RigidBody } from '@react-three/rapier';
-import { Box, useGLTF } from '@react-three/drei';
+import React, { useRef } from 'react';
+import { RigidBody } from '@react-three/rapier';
+import { Box, Image, Text } from '@react-three/drei';
 import Lights from './Lights';
 
 const Ground = ({ positionZ = 0, material }) => {
@@ -11,24 +11,60 @@ const Ground = ({ positionZ = 0, material }) => {
   );
 };
 
-const Wall = ({ positionZ = 0, material }) => {
+const Wall = ({ positionZ = 0, material, timeline, logo }) => {
+  const wallRef = useRef();
+  const imageRef = useRef();
+
   return (
     <RigidBody type="fixed">
       <Box
+        ref={wallRef}
         args={[0.25, 4, 8]}
         position={[2.875, 2.125, positionZ * 8]}
         material={material}
         castShadow
       />
+
+      {/* LOGO */}
+      <Image
+        ref={imageRef}
+        url="/images/booleanLogo.png"
+        opacity={1}
+        position={[2.725, 3.275, positionZ * 8 - 1.5]}
+        rotation={[0, Math.PI * -0.5, 0]}
+        scale={[4, 0.9]}
+        receiveShadow
+        attach={wallRef.current}
+      />
+
+      {/* TIMELINE */}
+      <Text
+        position={[0, 0.1275, positionZ * 8 - 3.75]}
+        rotation={[Math.PI / 2, Math.PI, 0]}
+        attach={wallRef.current}
+        fontSize={0.35}
+        anchorX={2.675}
+        color="#051531"
+      >
+        {timeline}
+      </Text>
     </RigidBody>
   );
 };
 
-const Room = ({ positionZ = 0, roomGroundMaterial, roomWallMaterial, lightColor }) => {
+const Room = ({
+  positionZ = 0,
+  roomGroundMaterial,
+  roomWallMaterial,
+  lightColor,
+  logo,
+  timeline
+}) => {
+  console.log(logo);
   return (
     <>
       <Ground positionZ={positionZ} material={roomGroundMaterial} />
-      <Wall positionZ={positionZ} material={roomWallMaterial} />
+      <Wall positionZ={positionZ} material={roomWallMaterial} logo={logo} timeline={timeline} />
 
       {/* Light */}
       <Lights positionZ={positionZ} color={lightColor} />
