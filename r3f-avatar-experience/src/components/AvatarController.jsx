76ecc32080357b70a257504141c2f0334c892a87
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-07-06 12:27:36 +0200

    :sparkles: Reset and Fall

--- r3f-avatar-experience/src/components/AvatarController.jsx
+++ r3f-avatar-experience/src/components/AvatarController.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import * as THREE from 'three';
 import { useFrame } from '@react-three/fiber';
 import { useKeyboardControls } from '@react-three/drei';
@@ -17,6 +17,11 @@ const AvatarController = () => {
   const rigidBody = useRef();
   const avatar = useRef();
   const isOnFloor = useRef(true);
+
+  const [smoothCameraPosition] = useState(() => new THREE.Vector3(-15, 7, -10));
+  const [smoothCameraTarget] = useState(() => new THREE.Vector3());
+
+  const reset = () => {};
 
   /**
    * State Management
@@ -37,7 +42,7 @@ const AvatarController = () => {
   /**
    * Apply Forces
    */
-  useFrame((state) => {
+  useFrame((state, delta) => {
     const impulse = { x: 0, y: 0, z: 0 };
 
     /**
@@ -51,6 +56,16 @@ const AvatarController = () => {
      *
      */
     const linvel = rigidBody?.current?.linvel();
+
+    /**
+     * For CAMERA FOLLOW and FALLING animation
+     */
+    const avatarWorldPosition = avatar.current.getWorldPosition(new THREE.Vector3());
+    const targetLookAt = new THREE.Vector3(
+      avatarWorldPosition.x - 0.5,
+      avatarWorldPosition.y + 1,
+      avatarWorldPosition.z + 0.5
+    );
 
     /**
      * When it's true will calculate the
@@ -110,6 +125,22 @@ const AvatarController = () => {
     }
 
     /**
+     * Falling Animation
+     */
+    if (avatarWorldPosition.y < -0.8) {
+      setAvatarState('Fall');
+    }
+
+    /**
+     * Reset After Fall
+     */
+    if (avatarWorldPosition.y < -5) {
+      rigidBody.current.setTranslation({ x: 0, y: 0, z: 0 });
+      rigidBody.current.setLinvel({ x: 0, y: 0, z: 0 });
+      rigidBody.current.setAngvel({ x: 0, y: 0, z: 0 });
+    }
+
+    /**
      * Calculate the current angle the avatar
      * is hading to and rotate accordingly
      *
@@ -122,19 +153,28 @@ const AvatarController = () => {
     /**
      * CAMERA FOLLOW
      */
-    const avatarWorldPosition = avatar.current.getWorldPosition(new THREE.Vector3());
-    const targetLookAt = new THREE.Vector3(avatarWorldPosition.x, 0.75, avatarWorldPosition.z);
+    const cameraPosition = new THREE.Vector3();
+    cameraPosition.copy(avatarWorldPosition);
+    cameraPosition.x -= 15;
+    cameraPosition.y += 7;
+    cameraPosition.z -= 10;
 
-    state.camera.position.x = avatarWorldPosition.x - 15;
-    state.camera.position.z = avatarWorldPosition.z - 10;
+    const cameraTarget = new THREE.Vector3();
+    cameraTarget.copy(targetLookAt);
 
-    state.camera.lookAt(targetLookAt);
+    // Smoother camera
+    smoothCameraPosition.lerp(cameraPosition, 5 * delta);
+    smoothCameraTarget.lerp(cameraTarget, 5 * delta);
+
+    state.camera.position.copy(smoothCameraPosition);
+    state.camera.lookAt(smoothCameraTarget);
   });
 
   return (
     <group position-y={1}>
       <RigidBody
         ref={rigidBody}
+        // type="dynamic"
         colliders={false}
         enabledRotations={[false, false, false]}
         onCollisionEnter={() => {
