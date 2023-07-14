fbaa1c026e0466fe25e85cd9e94cf1a9e9773d09
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-07-14 14:59:56 +0200

    :tada: Setup first Scene

--- r3d-dissolve_effect/src/components/Experience.jsx
+++ r3d-dissolve_effect/src/components/Experience.jsx
@@ -1,13 +1,21 @@
-import { OrbitControls } from '@react-three/drei';
+import { ContactShadows, Environment, OrbitControls } from '@react-three/drei';
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
+
+      {/* Cube */}
       <mesh>
-        <meshNormalMaterial />
-        <boxBufferGeometry />
+        <boxGeometry />
+        <meshStandardMaterial color="#ffffff" />
       </mesh>
+
+      {/* Lights */}
+      <Environment preset="sunset" />
+
+      {/* Shadow */}
+      <ContactShadows position-y={-1} />
     </>
   );
 };
