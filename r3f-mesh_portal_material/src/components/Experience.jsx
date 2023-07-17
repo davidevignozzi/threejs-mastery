3421536594562dc94f3421612be2accb4941fcc9
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-07-17 15:42:07 +0200

    :sparkles: Blockadelabs World

--- r3f-mesh_portal_material/src/components/Experience.jsx
+++ r3f-mesh_portal_material/src/components/Experience.jsx
@@ -1,12 +1,21 @@
-import { OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
+import { Environment, OrbitControls, useTexture } from '@react-three/drei';
 
 export const Experience = () => {
+  /**
+   * Textures
+   */
+  const map = useTexture('textures/anime_water_world.jpg');
+
   return (
     <>
+      <ambientLight intensity={0.5} />
+      <Environment preset="sunset" />
       <OrbitControls />
+
       <mesh>
-        <meshNormalMaterial />
-        <boxBufferGeometry />
+        <sphereGeometry args={[5, 64, 64]} />
+        <meshStandardMaterial map={map} side={THREE.BackSide} />
       </mesh>
     </>
   );
