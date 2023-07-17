c557a1eb34a5c7263a87121eb49d1096f5721d23
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-07-17 16:09:23 +0200

    :sparkles: New Cards Cactoro & Dragon_Evolved

--- r3f-mesh_portal_material/src/components/Experience.jsx
+++ r3f-mesh_portal_material/src/components/Experience.jsx
@@ -7,30 +7,57 @@ import {
   useTexture
 } from '@react-three/drei';
 import { Fish } from './Fish';
+import { Dragon_Evolved } from './Dragon_Evolved';
+import { Cactoro } from './Cactoro';
 
 export const Experience = () => {
-  /**
-   * Textures
-   */
-  const map = useTexture('textures/anime_water_world.jpg');
-
   return (
     <>
       <ambientLight intensity={0.5} />
       <Environment preset="sunset" />
       <OrbitControls />
+      <MonsterStage texture={'textures/anime_water_world.jpg'}>
+        <Fish scale={0.6} position-y={-1} />
+      </MonsterStage>
 
+      <MonsterStage
+        texture={'textures/anime_erupting_volcano_world.jpg'}
+        position-x={-2.5}
+        rotation-y={Math.PI / 8}
+      >
+        <Dragon_Evolved scale={0.5} position-y={-1} />
+      </MonsterStage>
+
+      <MonsterStage
+        texture={'textures/anime_cactus_forest_world.jpg'}
+        position-x={2.5}
+        rotation-y={-Math.PI / 8}
+      >
+        <Cactoro scale={0.45} position-y={-1} />
+      </MonsterStage>
+    </>
+  );
+};
+
+const MonsterStage = ({ children, texture, ...props }) => {
+  /**
+   * Texture
+   */
+  const map = useTexture(texture);
+
+  return (
+    <group {...props}>
       <RoundedBox args={[2, 3, 0.1]}>
         <MeshPortalMaterial side={THREE.DoubleSide}>
           <ambientLight intensity={1} />
           <Environment preset="sunset" />
-          <Fish scale={0.6} position-y={-1} />
+          {children}
           <mesh>
             <sphereGeometry args={[5, 64, 64]} />
             <meshStandardMaterial map={map} side={THREE.BackSide} />
           </mesh>
         </MeshPortalMaterial>
       </RoundedBox>
-    </>
+    </group>
   );
 };
