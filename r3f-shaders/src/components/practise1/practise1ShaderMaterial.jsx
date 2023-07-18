c36cd8b192d7e6cad7da7a11268cb0fe1a754a74
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-07-18 15:53:06 +0200

    :recycle: ShaderMaterail Basic Setup refactored

--- r3f-shaders/src/components/practise1/practise1ShaderMaterial.jsx
+++ r3f-shaders/src/components/practise1/practise1ShaderMaterial.jsx
@@ -1,5 +1,7 @@
 import { extend } from '@react-three/fiber';
 import { shaderMaterial } from '@react-three/drei';
+import vertexShader from './shaders/vertex.glsl';
+import fragmentShader from './shaders/fragment.glsl';
 
 const Practise1ShaderMaterial = shaderMaterial(
   /**
@@ -10,22 +12,12 @@ const Practise1ShaderMaterial = shaderMaterial(
   /**
    * Vertex Shader
    */
-  /* glsl */
-  `
-      void main() {
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);  
-      }
-    `,
+  vertexShader,
 
   /**
    * Fragment Shader
    */
-  /* glsl */
-  `
-      void main() {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-      }
-    `
+  fragmentShader
 );
 
 extend({ Practise1ShaderMaterial });
