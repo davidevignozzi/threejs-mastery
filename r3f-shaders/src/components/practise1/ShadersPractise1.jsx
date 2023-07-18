d5a2eca5c605c885f7cbb414d20aff92adf82484
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-07-18 16:42:45 +0200

    :bulb: Understand attributes

--- r3f-shaders/src/components/practise1/ShadersPractise1.jsx
+++ r3f-shaders/src/components/practise1/ShadersPractise1.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import Practise1ShaderMaterial from './practise1ShaderMaterial';
 
 const ShadersPractise1 = () => {
+  const planeRef = useRef();
+
+  useEffect(() => {
+    const geometry = planeRef?.current?.geometry;
+
+    // ? -- Attributes are values that change between each vertex
+    const attributes = geometry?.attributes;
+
+    const count = attributes?.position?.count;
+    const randoms = new Float32Array(count);
+
+    for (let i = 0; i < count; i++) {
+      randoms[i] = Math.random();
+    }
+
+    geometry?.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1));
+  }, []);
+
   return (
     <group>
-      <mesh>
+      <mesh ref={planeRef}>
         <planeGeometry args={[1, 1, 64, 64]} />
         <practise1ShaderMaterial
           side={THREE.DoubleSide}
--- r3f-shaders/src/components/practise1/shaders/vertex.glsl
+++ r3f-shaders/src/components/practise1/shaders/vertex.glsl
@@ -6,13 +6,17 @@
 
 // ? viewMatrix and the modelMatrix are combined into a modelViewMatrix 
 
+// ? -- Attributes are values that change between each vertex
+attribute float aRandom;
+
 void main() {
 
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-    // Flag effect -- Start
-    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
-    // Flag effect -- End
+    // * Flag effect -- Start
+    // modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+    modelPosition.z += aRandom * 0.1;
+    // * Flag effect -- End
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
