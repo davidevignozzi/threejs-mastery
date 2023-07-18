b1e616c3f6330f82e458fd3e8869c44b75c6a3a4
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-07-18 15:50:11 +0200

    :recycle: Refactor

--- r3f-shaders/src/components/practise1/ShadersPractise1.jsx
+++ r3f-shaders/src/components/practise1/ShadersPractise1.jsx
@@ -1,34 +1,4 @@
-import { shaderMaterial } from '@react-three/drei';
-import { extend } from '@react-three/fiber';
-
-const Practise1ShaderMaterial = shaderMaterial(
-  /**
-   * Uniforms
-   */
-  {},
-
-  /**
-   * Vertex Shader
-   */
-  /* glsl */
-  `
-    void main() {
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);  
-    }
-  `,
-
-  /**
-   * Fragment Shader
-   */
-  /* glsl */
-  `
-    void main() {
-      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    }
-  `
-);
-
-extend({ Practise1ShaderMaterial });
+import Practise1ShaderMaterial from './practise1ShaderMaterial';
 
 const ShadersPractise1 = () => {
   return (
--- r3f-shaders/src/components/practise1/practise1ShaderMaterial.jsx
+++ r3f-shaders/src/components/practise1/practise1ShaderMaterial.jsx
@@ -1,4 +1,33 @@
 import { extend } from '@react-three/fiber';
 import { shaderMaterial } from '@react-three/drei';
 
-export default practise1ShaderMaterial;
+const Practise1ShaderMaterial = shaderMaterial(
+  /**
+   * Uniforms
+   */
+  {},
+
+  /**
+   * Vertex Shader
+   */
+  /* glsl */
+  `
+      void main() {
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);  
+      }
+    `,
+
+  /**
+   * Fragment Shader
+   */
+  /* glsl */
+  `
+      void main() {
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+      }
+    `
+);
+
+extend({ Practise1ShaderMaterial });
+
+export default Practise1ShaderMaterial;
