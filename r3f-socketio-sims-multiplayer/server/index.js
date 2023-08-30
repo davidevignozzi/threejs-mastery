30ad1bbbb42f8bfc9447506a92e1792f0f4e365b
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-08-30 15:37:44 +0200

    :sparkles: Pathfinding setup

--- r3f-socketio-sims-multiplayer/server/index.js
+++ r3f-socketio-sims-multiplayer/server/index.js
@@ -1,4 +1,5 @@
 import { Server } from 'socket.io';
+import pathfinding from 'pathfinding';
 
 const io = new Server({
   cors: {
@@ -416,6 +417,60 @@ const map = {
     }
   ]
 };
+
+const grid = new pathfinding.Grid(
+  map.size[0] * map.gridDivision, // width
+  map.size[1] * map.gridDivision // height
+);
+
+const finder = new pathfinding.AStarFinder({
+  allowDiagonal: true,
+  dontCrossCorners: true
+});
+
+const findPath = (start, end) => {
+  const gridBackup = grid.clone();
+  const path = finder.findPath(
+    start[0],
+    start[1],
+    end[0],
+    end[1],
+    gridBackup
+  );
+
+  return path;
+};
+
+const updateGrid = () => {
+  map.items.forEach((item) => {
+    if (item.walkable || item.wall) {
+      return;
+    }
+
+    const width =
+      item.rotation === 1 || item.rotation === 3
+        ? item.size[1]
+        : item.size[0];
+    const height =
+      item.rotation === 1 || item.rotation === 3
+        ? item.size[0]
+        : item.size[1];
+
+    for (let x = 0; x < width; x++) {
+      for (let y = 0; y < height; y++) {
+        grid.setWalkableAt(
+          item.gridPosition[0] + x,
+          item.gridPosition[1] + y,
+          false
+        );
+      }
+    }
+  });
+};
+
+updateGrid();
+
+console.log(findPath([3, 3], [4, 7]));
 
 const generateRandomPosition = () => {
   return [Math.random() * map.size[0], 0, Math.random() * map.size[1]];
